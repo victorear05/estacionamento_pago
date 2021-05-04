683cca04e7cbec6dbd14f95cd3e65e70ed51a25c
Victor <victorear05@gmail.com>
2021-05-04 10:43:20 -0300

    Atualizando 1ª entrega

--- estacionamento_pago/src/app/Principal.java
+++ estacionamento_pago/src/app/Principal.java
@@ -1,8 +1,6 @@
 package app;
 
 import cadastros.*;
-import java.time.LocalDateTime;
-import javax.swing.JOptionPane;
 
 public class Principal {
 	
@@ -10,216 +8,21 @@ public class Principal {
 	static Veiculo cadV[] = new Veiculo[0];
 	static Acessos aces[] = new Acessos[0];
 	
-	public static void main(String[] args) {
-		menu();
-	}
+	public static void main(String[] args) {}
 	
-	public static void menu() {
-		
-		int opcao = 0;
-		
-		String menu = "Digite a opção desejada:\n"
-				+ "1 - Nova entrada simples\n" 		//OK
-				+ "2 - Entrada de mensalista\n"		//OK
-				+ "0 - Encerrar Programa\n";
-		do { 
-			String strOpcao = JOptionPane.showInputDialog(menu);
-			opcao = Integer.parseInt(strOpcao);
-			
-			switch(opcao) {
-				
-				case 1: {
-					int op;
-					String Menu = "Para veículo já cadastrado, digite 1\n"
-							+ "Para cadastrar um novo veículo, digite 2\n";		
-					String strOp = JOptionPane.showInputDialog(Menu);				
-					op = Integer.parseInt(strOp);
-					switch (op){
-						case 1: {
-							String placa;
-							placa = JOptionPane.showInputDialog("Digite a placa do veículo para busca");
-							Veiculo v = pesquisarVeiculo(placa);
-							novoAcesso(v);
-							break;
-						}
-						case 2: {
-							cadastrarVeiculoM(null);
-							break;
-						}
-						default: {
-							JOptionPane.showMessageDialog(null, "Opção inválida!\n");
-							break;				
-						}
-					}
-				}
-				
-				case 2: {
-					int op;
-
-					String Menu = ("Para usuário já cadastrado, digite 1\n"
-							+ "Para cadastrar um novo usuário, digite 2\n"
-							+ "Para cadastar um novo veículo de mensalista, digite 3\n");
-					String strOp = JOptionPane.showInputDialog(Menu);
-					op = Integer.parseInt(strOp);
-					switch(op){
-						case 1: {
-							pesquisarVMensalista();
-							break;	
-						}
-
-						case 2: {
-							cadastrarMensalista();
-							break;
-						}
-						
-						case 3: {
-							Mensalista M;
-							String CNH;
-							CNH = JOptionPane.showInputDialog("Digite a CNH a ser buscada:");
-							M = pesquisarMensalista(CNH);
-							cadastrarVeiculoM(M);
-							break;
-						}
-						
-						default: {
-							JOptionPane.showMessageDialog(null, "Opção inválida, voltando ao menu!");
-							break;				
-						}
-					}	
-				}
-				
-			}
-			
-		}while(opcao != 0);
-	}
+	public static void menu() {}
 	
-	public static void cadastrarVeiculoM(Mensalista mensalista) {
-		//Registrando Veículo:
-		String Marca = JOptionPane.showInputDialog("Marca:\n");
-		String Modelo = JOptionPane.showInputDialog("Modelo:\n");
-		String NPlaca = JOptionPane.showInputDialog("Número da Placa:\n");
-		
-		Veiculo v = new Veiculo(Marca, Modelo, NPlaca, mensalista);
-		Veiculo tempV[] = new Veiculo[cadV.length + 1];	
-		for(int i = 0; i < cadV.length;i++) {
-			tempV[i] = cadV[i];
-		}
-		tempV[tempV.length -1] = v;
-		cadV = tempV;
-		
-		//Registrando Acesso: 
-		String DEntrada = JOptionPane.showInputDialog("Digite a data de entrada:\n");
-		String HEntrada = JOptionPane.showInputDialog("Digite a hora de entrada:\n");
-		String DSaida = JOptionPane.showInputDialog("Digite a data de saída:\n");
-		String HSaida = JOptionPane.showInputDialog("Digite a hora de saída:\n");
-		LocalDateTime entrada;
-		LocalDateTime saida;
-		
-		Acessos A = new Acessos(v, entrada, saida);    	//Tranformar String em LocalDateTime
-		Acessos tempA[] = new Acessos[aces.length + 1];	
-		for(int i = 0; i < aces.length;i++) {
-			tempA[i] = aces[i];
-		}
-		tempA[tempA.length -1] = A;
-		aces = tempA;
+	public static void cadastrarVeiculoM(Mensalista mensalista) {} 		
 	
-	}
+	public static void cadastrarMensalista() {}							
 	
-	public static void cadastrarMensalista() {
-		
-		String nome = JOptionPane.showInputDialog("Nome:\n");
-		String endereco = JOptionPane.showInputDialog("Endereco:\n");
-		String celular = JOptionPane.showInputDialog("Celular:\n");
-		String telefone = JOptionPane.showInputDialog("Telefone:\n");
-		String CNH = JOptionPane.showInputDialog("CNH:\n");
-		
-		Mensalista M = new Mensalista(nome, endereco, celular, telefone, CNH);
-		Mensalista tempM[] = new Mensalista[cadM.length + 1];	
-		for(int i = 0; i < cadM.length;i++) {
-			tempM[i] = cadM[i];
-		}
-		tempM[tempM.length -1] = M;
-		cadM = tempM;
-		
-		cadastrarVeiculoM(M);
-	}
+	public static Veiculo pesquisarVeiculo(String placa) {return null;} 
 	
-	public static Veiculo pesquisarVeiculo(String placa) {
-		for(int i = 0; i < cadM.length; i++) {
-			if(placa == cadV[i].getPlaca())
-				return cadV[0];
-		}
-		return null;	
-	}
+	public static Mensalista pesquisarMensalista(String CNH) {return null;} 
 	
-	public static Mensalista pesquisarMensalista(String CNH) {
-		for(int i = 0; i < cadM.length; i++) {
-			if(CNH == cadM[i].getCNH())
-				return cadM[0];
-		}
-		return null;	
-	}
-
-	public static void novoAcesso(Veiculo v) {
-		
-		String DEntrada = JOptionPane.showInputDialog("Digite a data de entrada:\n");
-		String HEntrada = JOptionPane.showInputDialog("Digite a hora de entrada:\n");
-		String DSaida = JOptionPane.showInputDialog("Digite a data de saída:\n");
-		String HSaida = JOptionPane.showInputDialog("Digite a hora de saída:\n");
-		LocalDateTime entrada;
-		LocalDateTime saida;
-		
-		Acessos A = new Acessos(v, entrada, saida);		//Tranformar String em LocalDateTime
-		Acessos tempA[] = new Acessos[aces.length + 1];	
-		for(int i = 0; i < aces.length;i++) {
-			tempA[i] = aces[i];
-		}
-		tempA[tempA.length -1] = A;
-		aces = tempA;
-	}
+	public static void novoAcesso(Veiculo v) {}
 	
-	public static void pesquisarVMensalista() {
-		
-		Mensalista M = null;
-		String CNH;
-		int i= 0;
-		CNH = JOptionPane.showInputDialog("Digite o CPF do usuário para busca");
-		while(i < cadM.length) {
-			if(CNH == cadM[i].getCNH())
-				M = cadM[i];
-			else	
-				i++;
-		}	
-		if(M != null) {
-			Veiculo VM[] = M.getCadVM();
-			if(VM.length > 0) {
-				int opV;
-				for(i = 0; i < VM.length; i++)
-					JOptionPane.showMessageDialog(null,"Veículo " + i + ": \n"  
-													  +	"Marca: " + VM[i].getMarca() + "\n"
-													  + "Modelo: " + VM[i].getModelo()+ "\n"
-													  + "Placa: " + VM[i].getPlaca());
-				
-				String strOpV = JOptionPane.showInputDialog("Para qual carro deseja registrar um acesso?");
-				opV = Integer.parseInt(strOpV);
-				novoAcesso(VM[opV]);
-				JOptionPane.showMessageDialog(null, "Acesso Registrado!");
-			}
-			else
-				JOptionPane.showMessageDialog(null, "Usuário não possui veículos cadastrados");
-		}
-		else
-			JOptionPane.showMessageDialog(null, "Usuário não encontrado!");
-	}
+	public static void pesquisarVMensalista() {}
 	
-	public void imprimiAcessos() {
-		for(int i = 0;i < aces.length;i++) {
-			JOptionPane.showMessageDialog(null, aces[i].getV().getMarca() + "\n" 
-											  + aces[i].getV().getModelo() + "\n" 
-											  + aces[i].getV().getPlaca() + "\n"
-											  + aces[i].getEntrada() + "\n"
-											  + aces[i].getSaida() + "\n"
-											  + aces[i].getPreco());
-		}
-	}
+	public void imprimiAcessos() {}
 }
--- estacionamento_pago/src/cadastros/Acessos.java
+++ estacionamento_pago/src/cadastros/Acessos.java
@@ -21,28 +21,7 @@ public class Acessos {
 		
 	}
 
-	public void calcularPreco() {
-		
-		Duration duracao = Duration.between(entrada, saida);
-		long duracao_h = duracao.toHours();
-		long duracao_m = duracao.toMinutes();
-		
-		if(v.getMensalista() != null) {
-			p[0] = new FMensalista(duracao_m);
-		}else if (duracao_m == 0) {  			 //Implementar condição do if para caso seja pernoite
-			p[0] = new FPernoite(duracao_m);
-		}else if (duracao_m < 15) {
-			p[0] = new FMinuto(duracao_m);
-		}else if(duracao_m < 60) {
-			p[0] = new FQuinze(duracao_m);
-		}else if(duracao_h < 9) {
-			p[0] = new FHora(duracao_m);
-		}else if(duracao_h >= 9) {
-			p[0] = new FDiaria(duracao_m);
-		}
-		
-		setPreco(p[0].calculoDoValor());
-	}
+	public void calcularPreco() {setPreco(p[0].calculoDoValor());}
 
 	public void setPreco(double preco) {
 		this.preco = preco;
